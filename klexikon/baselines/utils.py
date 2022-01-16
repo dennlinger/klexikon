1908d3563968a5c270a96e6e6b8ae4b53b2ce053
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-01-16 08:35:11 +0100

    Adding directory iterator util.

---
+++ klexikon/baselines/utils.py
@@ -0,0 +1,13 @@
+"""
+Simply utility to iterate through a directory containing text files.
+"""
+from typing import IO
+import os
+
+
+def directory_iterator(source_dir: str = "./data/raw/wiki/", target_dir: str = "./data/baselines/lead_3/") -> (IO, IO):
+    for fn in sorted(os.listdir(source_dir)):
+        in_fp = os.path.join(source_dir, fn)
+        out_fp = os.path.join(target_dir, fn)
+
+        yield in_fp, out_fp
