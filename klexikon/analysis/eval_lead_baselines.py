c0f02978f78e3b6d0e49cc1b1d33a16308a64e95
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-03-07 15:16:37 +0100

    Reverting changes from ROUGE-2 experiments. Also ensuring that the same compound treatment is applied to all values.

--- klexikon/analysis/eval_lead_baselines.py
+++ klexikon/analysis/eval_lead_baselines.py
@@ -1,13 +1,8 @@
 """
-Compare results with Rouge.
+Compute baseline ROUGE scores
 """
-
-import numpy as np
-from tqdm import tqdm
-
-from rouge_score import rouge_scorer
 from rouge_score.scoring import BootstrapAggregator
-from .utils import print_aggregate, evaluate_directory, directory_iterator
+from .utils import print_aggregate, evaluate_directory
 from ..baselines.baselines import get_rouge_scorer_with_cistem
 
 
@@ -18,60 +13,42 @@ def f1_score(score):
 
 if __name__ == "__main__":
 
-    # aggregator = BootstrapAggregator(confidence_interval=0.95)
-    # # scorer = get_rouge_scorer_with_cistem()
+    aggregator = BootstrapAggregator(confidence_interval=0.95)
+    scorer = get_rouge_scorer_with_cistem()
     # scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
-    #
-    # evaluate_directory(aggregator, scorer, pred_dir="./data/baselines/lead_3", gold_dir="./data/gold/")
-    # print("\n------------------------------------")
-    # print("Results for Lead-3 baseline:")
-    # result = aggregator.aggregate()
-    # print_aggregate(result)
-    #
-    # aggregator = BootstrapAggregator(confidence_interval=0.95)
-    # scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
-    #
-    # evaluate_directory(aggregator, scorer, pred_dir="./data/baselines/lead_k", gold_dir="./data/gold/")
-    # print("\n------------------------------------")
-    # print("Results for Lead-k baseline (Wikipedia's 'summary section'):")
-    # result = aggregator.aggregate()
-    # print_aggregate(result)
-    #
-    # aggregator = BootstrapAggregator(confidence_interval=0.95)
-    # scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
-    #
-    # evaluate_directory(aggregator, scorer, pred_dir="./data/baselines/lead_k_simplified", gold_dir="./data/gold/")
-    # print("\n------------------------------------")
-    # print("Results for Lead-k-simplified baseline (Wikipedia's 'summary section minus bracket texts'):")
-    # result = aggregator.aggregate()
-    # print_aggregate(result)
+
+    evaluate_directory(aggregator, scorer, pred_dir="./data/baselines/lead_3", gold_dir="./data/gold/")
+    print("\n------------------------------------")
+    print("Results for Lead-3 baseline:")
+    result = aggregator.aggregate()
+    print_aggregate(result)
 
     aggregator = BootstrapAggregator(confidence_interval=0.95)
-    scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
+    scorer = get_rouge_scorer_with_cistem()
+    # scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
 
-    # evaluate_directory(aggregator, scorer, pred_dir="./data/summaries", gold_dir="./data/gold/")
-    # print("\n------------------------------------")
-    # print("Results for LexRank/S-Transformer-simplified baseline:")
-    # result = aggregator.aggregate()
-    # print_aggregate(result)
+    evaluate_directory(aggregator, scorer, pred_dir="./data/baselines/lead_k", gold_dir="./data/gold/")
+    print("\n------------------------------------")
+    print("Results for Lead-k baseline (Wikipedia's 'summary section'):")
+    result = aggregator.aggregate()
+    print_aggregate(result)
 
-    results = []
-    for pred_fp, gold_fp in tqdm(directory_iterator("./data/summaries", "./data/gold")):
-        with open(gold_fp) as f:
-            gold = "".join(f.readlines())
-        with open(pred_fp) as f:
-            pred = "".join(f.readlines())
+    aggregator = BootstrapAggregator(confidence_interval=0.95)
+    scorer = get_rouge_scorer_with_cistem()
+    # scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
 
-        results.append(scorer.score(gold, pred)["rouge1"])
+    evaluate_directory(aggregator, scorer, pred_dir="./data/baselines/lead_k_simplified", gold_dir="./data/gold/")
+    print("\n------------------------------------")
+    print("Results for Lead-k-simplified baseline (Wikipedia's 'summary section minus bracket texts'):")
+    result = aggregator.aggregate()
+    print_aggregate(result)
 
-    for result in results:
-        f1_score(result)
+    aggregator = BootstrapAggregator(confidence_interval=0.95)
+    scorer = get_rouge_scorer_with_cistem()
+    # scorer = rouge_scorer.RougeScorer(["rouge1", "rouge2", "rougeL"], use_stemmer=False)
 
-    precisions = [score.precision for score in results]
-    recalls = [score.recall for score in results]
-    f1measures = [score.fmeasure for score in results]
-    print(np.mean(precisions))
-    print(np.mean(recalls))
-    print(np.mean(f1measures))
-    print(2 * (np.mean(precisions) * np.mean(recalls) / (np.mean(precisions) + np.mean(recalls))))
-
+    evaluate_directory(aggregator, scorer, pred_dir="./data/summaries", gold_dir="./data/gold/")
+    print("\n------------------------------------")
+    print("Results for LexRank/S-Transformer-simplified baseline:")
+    result = aggregator.aggregate()
+    print_aggregate(result)
